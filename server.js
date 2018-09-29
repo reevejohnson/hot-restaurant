18ef203666c1d6e43e73ddd8c571764bd2a531fd
Reeve Johnson <reevewjohnson@gmail.com>
2018-09-29 11:01:54 -0500

    added javascript and initiliazed node

---
+++ server.js
@@ -0,0 +1,67 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Reservations (DATA)
+// =============================================================
+var reservations = [];
+var waitlist = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// NEED ON-CLICK EVENT
+if(reservations.length < 5) {
+    app.post("/api/tables", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newreservation = req.body;
+      
+        console.log(newreservation);
+      
+        reservations.push(newreservation);
+      
+        res.json(newreservation);
+    });
+} else {
+    app.post("/api/waitlist", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newreservation = req.body;
+      
+        console.log(newreservation);
+      
+        waitlist.push(newreservation);
+      
+        res.json(newreservation);
+    });
+}
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
